812196d9165b343cbcb8e85a1a02b20d84bc6093
Ananthu M A <heartchaserananthu@gmail.com>
2023-10-14 01:07:34 +0530

    session managed

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'the_smart_depot',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
